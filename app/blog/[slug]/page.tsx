3c6e4974cf39358b23b87e1a3a629694ca2e7c68
tatsuyaH20 <tatsuya.hirano@askul.com>
2023-11-03 01:05:52 +0900

    Update：nextバージョンアップ

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,6 +1,6 @@
 export const dummyData = [
-  { slug: '1111', title: 'dummy1' },
-  { slug: '2222', title: 'dummy2' },
+  { slug: '1111', title: 'タイトル1タイトル1タイトル1タイトル1' },
+  { slug: '2222', title: 'タイトル2' },
 ];
 
 export const generateMetadata = async ({ params }: { params: { slug: string } }) => {
